d09faa1526dc2930666755a757333edcfae17eef
karkaih <achrafkarkaih@gmail.com>
2022-05-17 19:41:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/roomwordssample/WordViewModel.java
@@ -0,0 +1,51 @@
+package com.example.android.roomwordssample;
+
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+/**
+ * View Model to keep a reference to the word repository and
+ * an up-to-date list of all words.
+ */
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository mRepository;
+
+    public WordRepository getmRepository() {
+        return mRepository;
+    }
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+    public void update(Word word) { mRepository.update(word); }
+    public void deleteByName(Word word) { mRepository.deleteByName(word); }
+}
