1c9580f1ea77be950f4f3dc2c40edd0094687b31
karkaih <achrafkarkaih@gmail.com>
2022-05-17 19:44:50 +0100

    TP Room

--- app/src/main/java/com/example/android/roomwordssample/WordRepository.java
+++ app/src/main/java/com/example/android/roomwordssample/WordRepository.java
@@ -1,20 +1,6 @@
 package com.example.android.roomwordssample;
 
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 import android.app.Application;
 import androidx.lifecycle.LiveData;
@@ -32,25 +18,19 @@ public class WordRepository {
     private final WordDao mWordDao;
     private final LiveData<List<Word>> mAllWords;
 
-    // Note that in order to unit test the WordRepository, you have to remove the Application
-    // dependency. This adds complexity and much more code, and this sample is not about testing.
-    // See the BasicSample in the android-architecture-components repository at
-    // https://github.com/googlesamples
+
     WordRepository(Application application) {
         WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
         mWordDao = db.wordDao();
         mAllWords = mWordDao.getAlphabetizedWords();
     }
 
-    // Room executes all queries on a separate thread.
-    // Observed LiveData will notify the observer when the data has changed.
+
     LiveData<List<Word>> getAllWords() {
         return mAllWords;
     }
 
-    // You must call this on a non-UI thread or your app will crash.
-    // Like this, Room ensures that you're not doing any long running operations on the main
-    // thread, blocking the UI.
+
     public void insert (Word word) {
         new insertAsyncTask(mWordDao).execute(word);
     }
--- app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.java
@@ -1,20 +1,5 @@
 package com.example.android.roomwordssample;
 
-/*
- * Copyright (C) 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.room.Database;
@@ -24,10 +9,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import androidx.annotation.NonNull;
 
-/**
- * This is the backend. The database. This used to be done by the OpenHelper.
- * The fact that this has very few comments emphasizes its coolness.
- */
+
 
 @Database(entities = {Word.class}, version = 1,  exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -53,10 +35,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         return INSTANCE;
     }
 
-    /**
-     * Override the onOpen method to populate the database.
-     * For this sample, we clear the database every time it is created or opened.
-     */
+
     private static final RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
 
         @Override
@@ -68,10 +47,7 @@ public abstract class WordRoomDatabase extends RoomDatabase {
         }
     };
 
-    /**
-     * Populate the database in the background.
-     * If you want to start with more words, just add them.
-     */
+
     private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
         private final WordDao mDao;
